4c3b91b21276cf426f107e3f9fe1b89eb0dd6dd0
elliotgonzalez123 <elliotgrey@gmail.com>
2023-03-21 15:31:34 -0400

    remove ipv4 config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
-const dns = require('dns')
+// const dns = require('dns')
 
-dns.setDefaultResultOrder("ipv4first")
+// dns.setDefaultResultOrder("ipv4first")
 
 const nextConfig = {
   experimental: {
